db1ce25fbaa99e74274879390c37e7f1203fc0f5
SwatiKatigar789 <96766607+SwatiKatigar789@users.noreply.github.com>
2023-06-03 15:51:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: 'root',
+    user: 'mmuser11',
+    password: 'MMuser+=123',
     database: 'Doctor_Appointment'
 });
 
